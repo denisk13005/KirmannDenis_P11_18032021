bfecec506da9bad1d74515a0dbdce1c878e26efe
kirmann <k.dkle@hotmail.fr>
2022-02-08 22:48:42 +0100

    error page desktop ok + header mobile ok

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import Header from '../components/Header'
 import '../styles/pages/errorPage.scss'
 
 const ErrorPage = () => {
   return (
-    <div>
-      <h1>Page not found</h1>
+    <div className="error__container">
+      <Header />
+      <h1>404</h1>
+      <p>Oups! La page que vous demandez n'existe pas.</p>
+      <NavLink to="/">
+        <li className="return">Retourner sur la page d'accueil</li>
+      </NavLink>
     </div>
   )
 }
