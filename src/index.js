29018cfe67d5e0946d44c7df936796000fa62363
kirmann <k.dkle@hotmail.fr>
2022-02-08 12:52:40 +0100

    mise en place des style

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import Home from './pages/Home'
 import ErrorPage from './pages/ErrorPage'
-import './styles/index.css'
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import '../styles/pages/home.scss'
 const Home = () => {
   return (
     <div>
