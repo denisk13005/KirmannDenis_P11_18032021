dccac8406319acbeb35a260f99ee689033827603
kirmann <k.dkle@hotmail.fr>
2022-02-09 08:35:26 +0100

    footer + app.js

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import Home from './pages/Home'
+import ErrorPage from './pages/ErrorPage'
+import About from './pages/About'
+import Accommodation from './pages/Accommodation'
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/accommodation" element={<Accommodation />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
