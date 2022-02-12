7a087bc9ded4b209b9d2c655539b539dbc4f3ab6
kirmann <k.dkle@hotmail.fr>
2022-02-12 22:54:01 +0100

    mode mobile page accommodation ok

--- src/components/RentalInformations.jsx
+++ src/components/RentalInformations.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import AboutContainer from './AboutContainer'
+import greyStar from '../assets/greyStar.svg'
+import pinkStar from '../assets/pinkStar.svg'
 
 const RentalInformations = ({ accommodation }) => {
   // for (let i = 0; i < accommodation.rating; i++) {
@@ -8,23 +10,31 @@ const RentalInformations = ({ accommodation }) => {
   // console.log(accommodation.rating)
   return (
     <div className="rentalInformations">
-      <div className="accommodationTitleAndImg">
-        <div className="accommodationTitleAndImg__title">
-          <h1>{accommodation.title}</h1>
-          <h2>{accommodation.location}</h2>
+      <div className="informations">
+        <div className="accommodationTitleAndImg">
+          <div className="accommodationTitleAndImg__title">
+            <h1>{accommodation.title}</h1>
+            <h2>{accommodation.location}</h2>
+          </div>
+          <div className="accommodationTitleAndImg__img">
+            <h2>{accommodation.host.name}</h2>
+            <img src={accommodation.host.picture} alt="" />
+          </div>
         </div>
-        <div className="accommodationTitleAndImg__img">
-          <h2>{accommodation.host.name}</h2>
-          <img src={accommodation.host.picture} alt="" />
+        <div className="tagsAndStars">
+          <ul className="tags">
+            {accommodation.tags.map((el, index) => (
+              <li key={index}>{el}</li>
+            ))}
+          </ul>
+          <div className="stars">
+            <img src={pinkStar} alt="" />
+            <img src={pinkStar} alt="" />
+            <img src={pinkStar} alt="" />
+            <img src={greyStar} alt="" />
+            <img src={greyStar} alt="" />
+          </div>
         </div>
-      </div>
-      <div className="tagsAndStars">
-        <ul className="tags">
-          {accommodation.tags.map((el, index) => (
-            <li key={index}>{el}</li>
-          ))}
-        </ul>
-        <div className="stars">{}</div>
       </div>
       <div className="rantalContainers">
         <AboutContainer el={accommodation} />
