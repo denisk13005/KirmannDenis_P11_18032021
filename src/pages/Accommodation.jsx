91ca25984f53b170ae52c306b33d82a7e727eaac
kirmann <k.dkle@hotmail.fr>
2022-02-12 11:34:50 +0100

    début titre et image du logement

--- src/pages/Accommodation.jsx
+++ src/pages/Accommodation.jsx
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import AboutContainer from '../components/AboutContainer'
-import Footer from '../components/Footer'
 import Header from '../components/Header'
 import Lightbox from '../components/Lightbox'
 import '../styles/pages/accommodation.scss'
@@ -14,7 +12,7 @@ const Accommodation = () => {
       .then((data) =>
         setAccommodation(
           data.accommodations.find(
-            (el) => el.id === window.location.hash.slice(1)
+            (el) => el.id === window.location.hash.slice(1) // récupère le logement concerné par l'id dans l'url
           )
         )
       )
@@ -25,6 +23,16 @@ const Accommodation = () => {
     <div>
       <Header />
       <Lightbox accommodation={accommodation} />
+      <div className="accommodationTitleAndImg">
+        <div className="accommodationTitleAndImg__title">
+          <h1>{accommodation.title}</h1>
+          <h2>{accommodation.location}</h2>
+        </div>
+        <div className="accommodationTitleAndImg__img">
+          <h2>{accommodation.host.name}</h2>
+          <img src={accommodation.host.picture} alt="" />
+        </div>
+      </div>
 
       {/* <Footer /> */}
     </div>
