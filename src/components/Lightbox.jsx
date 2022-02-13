0ad753e72ffa3dc9d4ca44a0cae423ecd7a65137
kirmann <k.dkle@hotmail.fr>
2022-02-13 11:06:47 +0100

    lightbox ok

--- src/components/Lightbox.jsx
+++ src/components/Lightbox.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import rightArrow from '../assets/rightArrow.svg'
 import '../styles/components/lightbox.scss'
 
 const Lightbox = ({ accommodation }) => {
   console.log(accommodation.pictures)
+  let [currentIndex, setCurrentIndex] = useState(0)
+  console.log(currentIndex)
+  console.log(accommodation.pictures.length)
+
+  const next = () => {
+    setCurrentIndex(currentIndex + 1)
+    if (currentIndex === accommodation.pictures.length - 1) {
+      setCurrentIndex(0)
+    }
+  }
+  const previous = () => {
+    setCurrentIndex(currentIndex - 1)
+    if (currentIndex === 0) {
+      setCurrentIndex(accommodation.pictures.length - 1)
+    }
+  }
   return (
-    <div className="lightbox__container">
-      <img src={accommodation.cover} alt="" />
-    </div>
+    <>
+      <div className="lightbox__container">
+        <img src={accommodation.pictures[currentIndex]} alt="" />
+        <img onClick={next} className="rightArrow" src={rightArrow} alt="" />
+        <img onClick={previous} className="leftArrow" src={rightArrow} alt="" />
+      </div>
+    </>
   )
 }
 
