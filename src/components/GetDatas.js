dab7d441e97c992c39dfd681391e10e7c25a3010
kirmann <k.dkle@hotmail.fr>
2022-02-15 23:03:12 +0100

    clear timetout

--- src/components/GetDatas.js
+++ src/components/GetDatas.js
@@ -7,6 +7,7 @@ const GetDatas = () => {
         .then((res) => res.json())
         .then((datas) => setDatas(datas))
     }, 1000)
+    return () => clearTimeout(timer)
   }, [])
   let [datas, setDatas] = useState([])
   return datas
