8e69113fe6b411ac56dffbfbb4b9b5fb71c588c2
kirmann <k.dkle@hotmail.fr>
2022-02-23 14:52:06 +0100

    supression des commentaires et des console log

--- src/components/GetDatas.js
+++ src/components/GetDatas.js
@@ -2,22 +2,6 @@ const fetchDatas = async () => {
   const datas = await fetch('/data/datas.json')
     .then((res) => res.json())
     .then((data) => data)
-  console.log(datas)
   return datas
 }
 export default fetchDatas
-
-// const GetDatas = () => {
-//   let [datas, setDatas] = useState([])
-//   useEffect(() => {
-//     const timer = setTimeout(() => {
-//       fetch('/data/datas.json')
-//         .then((res) => res.json())
-//         .then((datas) => setDatas(datas))
-//     }, 100)
-//     return () => clearTimeout(timer)
-//   }, [])
-//   return datas
-// }
-
-// export default GetDatas
